ffac8e6968710a31b9280dcff392bd887a190cfd
D-TUCKER <darren.s.tucker@gmail.com>
2018-03-17 16:04:58 -0400

    a

--- app.py
+++ app.py
@@ -66,7 +66,8 @@ def send_message(recipient_id, message_text,wit_response):
     I once sat next to David Price on a flight from Toronto. 
     He got hammered on those little nips they give you, you know? 
     Booted in the little baggie. I’ve had better flights, I'll tell you that.
-    You can expect that he’ll only score about 0.78 points through 18 batters.
+    David Price is probably going to get hammered in Yankee Stadium.
+    You can expect that he'll only score about 0.78 points through 18 batters.
     """
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
