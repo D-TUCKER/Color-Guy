9b0b2b42fd210ed84bc21d51e1469e1118f613b6
D-TUCKER <darren.s.tucker@gmail.com>
2018-03-17 15:44:41 -0400

    does this work?

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    wit_response = json.loads(client.message(message_text))
+                    wit_response = json.loads(str(client.message(message_text)))
                     send_message(sender_id, "Juuuust a bit outside...",wit_response)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
